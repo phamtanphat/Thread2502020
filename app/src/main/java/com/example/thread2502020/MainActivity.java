8d1d2c5460d99e61904a71addaff5cd9a3795834
phatdroid94 <phatdroid94@gmail.com>
2020-10-31 20:52:34 +0700

    flag

--- app/src/main/java/com/example/thread2502020/MainActivity.java
+++ app/src/main/java/com/example/thread2502020/MainActivity.java
@@ -7,29 +7,49 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyFlag myFlag;
+    int a, b, c;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        a = b = c = 0;
 
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-               printCount("Thread A");
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 10; i++) {
+                        a = i;
+                        Log.d("BBB", "Giá trị A : " + a);
+                    }
+                }
             }
         });
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                printCount("Thread B");
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 10; i++) {
+                        b = i;
+                        Log.d("BBB", "Giá trị B : " + b);
+                    }
+                }
+            }
+        });
+        Thread threadC = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (myFlag) {
+                    for (int i = 1; i <= 10; i++) {
+                        c = a + b;
+                        Log.d("BBB", "Giá trị C : " + c);
+                    }
+                }
             }
         });
         threadA.start();
         threadB.start();
-    }
-    private synchronized void printCount(String name){
-        for (int i = 0; i < 1000 ; i++) {
-            Log.d("BBB",name + " : " +i);
-        }
     }
 }
